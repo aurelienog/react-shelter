422091863275f47edac418063f7e1bc0526f3939
Aurelie Nogueira <aurelienog@gmail.com>
2025-02-12 21:26:27 +0100

    feat navigation on click to animal detail

--- web/src/components/animals/animals-item/AnimalsItem.jsx
+++ web/src/components/animals/animals-item/AnimalsItem.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function AnimalItem({animal}) {
   return (
-    <article className='border'>
-      <img src={animal.img} alt="" />
-      <h3>{animal.name}</h3>
-      <p>{animal.sex}</p>
-      <p>{animal.age}</p>
-      <p>{animal.size}</p>
-    </article>
+    <NavLink to={`/animals/${animal.id}`}>
+      <article className='border'>
+        <img src={animal.img} alt="" />
+        <h3>{animal.name}</h3>
+        <p>{animal.sex}</p>
+        <p>{animal.age}</p>
+        <p>{animal.size}</p>
+      </article>
+    </NavLink>
+    
   )
 }
 
